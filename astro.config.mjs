0aa94f1b9dbe564e400ef8b996b90cff9f849c86
somethingxdev <143795997+somethingxdev@users.noreply.github.com>
2026-03-09 19:30:19 +0200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,38 @@
+import tailwindcss from '@tailwindcss/vite'
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig, fontProviders } from 'astro/config'
+import sveltia from 'astro-sveltia-cms'
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: 'Montserrat',
+        cssVariable: '--font-base',
+        formats: ['woff2'],
+        weights: [400, 500, 600, 700],
+        subsets: ['cyrillic'],
+      },
+      {
+        provider: fontProviders.local(),
+        name: 'Custom',
+        cssVariable: '--font-heading',
+        options: {
+          variants: [
+            {
+              weight: 400,
+              style: 'normal',
+              src: ['./src/assets/fonts/faberge-regular.otf'],
+            },
+          ],
+        },
+      },
+    ],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+})
