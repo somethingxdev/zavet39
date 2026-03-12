8f7497448ef510b706b3ffebc4c984a0c643969c
somethingxdev <143795997+somethingxdev@users.noreply.github.com>
2026-03-12 20:59:35 +0200

    update config

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,15 +3,16 @@ import { z } from 'astro/zod'
 import { glob } from 'astro/loaders'
 const monuments = defineCollection({
   loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/data/monuments' }),
-  schema: z.object({
-    title: z.string(),
-    image: z.string(),
-    materials: z.string(),
-    dimensions: z.string(),
-    price: z.string().optional(),
-    type: z.string(),
-    category: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      image: image(),
+      materials: z.string(),
+      dimensions: z.string(),
+      price: z.string().optional(),
+      type: z.string(),
+      category: z.string(),
+    }),
 })
 
 export const collections = {
