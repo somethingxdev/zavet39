be6bd323c32dbc575a319ab615c722b2a6c1cbf0
somethingxdev <143795997+somethingxdev@users.noreply.github.com>
2026-05-10 14:31:16 +0200

    test build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,10 @@
 import tailwindcss from '@tailwindcss/vite'
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config'
+import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zavet39.ru',
-
-  fonts: [
-    {
-      provider: fontProviders.fontsource(),
-      name: 'Montserrat',
-      cssVariable: '--font-base',
-      formats: ['woff2'],
-      weights: [400, 500, 600, 700],
-      subsets: ['cyrillic', 'latin'],
-    },
-    {
-      provider: fontProviders.local(),
-      name: 'Custom',
-      cssVariable: '--font-heading',
-      options: {
-        variants: [
-          {
-            weight: 400,
-            style: 'normal',
-            src: ['./src/assets/fonts/faberge-regular.otf'],
-          },
-        ],
-      },
-    },
-  ],
 
   vite: {
     plugins: [tailwindcss()],
